07903f1cb05f4480c055f590ee58bbdd503abc06
Ricardo Bono <83359117+BonoX23@users.noreply.github.com>
2022-11-01 16:33:00 -0300

    Propriedade Pessoa
    Criação da propriedade Pessoa

---
+++ Cadastro Api/Cadastro/Models/Cadastro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cadastro.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public char Sexo { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public string Cpf { get; set; }
+
+
+
+    }
+}
---
+++ Cadastro Api/Cadastro/Models/CadastroContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cadastro.Models
+{
+    public class PessoaContext : DbContext
+    {
+        public PessoaContext(DbContextOptions<PessoaContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Pessoa> Cadastro { get; set; }
+    }
+}
